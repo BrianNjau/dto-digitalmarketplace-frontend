3bcb8ab0e6dc855b55d687160dfeb6c6c0520ae6
Reshma-M-A <reshmamabraham@gmail.com>
2020-12-08 12:47:52 +1100

    wip

--- apps/marketplace/components/SellerAssessment/SellerAssessmentCriteriaStage.js
+++ apps/marketplace/components/SellerAssessment/SellerAssessmentCriteriaStage.js
@@ -25,8 +25,7 @@ const getMessage = (domain, maxDailyRate) => {
   const criteriaNeeded = getCriteriaNeeded(domain.criteriaNeeded, domain.priceMaximum, maxDailyRate)
   const essentialCriteria = domain.criteria.filter(criterion => criterion.essential)
   if (domain.name === 'Platforms integration') {
-    const platformMessage = `You must submit at least ${criteriaNeeded -
-      essentialCriteria.length} &apos; Other criteria &apos;`
+    const platformMessage = `You must submit at least ${criteriaNeeded - essentialCriteria.length}  'Other criteria'`
     return platformMessage
   }
   const message = `You must submit evidence for at least ${criteriaNeeded} ${
