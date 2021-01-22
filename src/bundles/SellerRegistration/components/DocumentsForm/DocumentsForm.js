47f2dcc7134d3ba1ed94699452447c265ff2b8e7
Reshma-M-A <reshmamabraham@gmail.com>
2021-01-23 10:22:50 +1100

    wip

--- src/bundles/SellerRegistration/components/DocumentsForm/DocumentsForm.js
+++ src/bundles/SellerRegistration/components/DocumentsForm/DocumentsForm.js
@@ -217,7 +217,7 @@ class DocumentsForm extends BaseForm {
                         onCustomSubmit={onSubmit}
                         onSubmitFailed={onSubmitFailed}
                         validators={{
-                            documents: checkNoOfDocuments(documents)
+                            documents: (documents = {}) => this.checkNoOfDocuments(documents) && documents.workers.noWorkersCompensation !== false
                         }}
                     >
                         {csrf_token && (
